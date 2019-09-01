01ed33cd8bf2ef6cc185905bcaa52a1047b1e971
小新哥 <403713133@qq.com>
2019-09-01 16:32:32 +0800

    .

---
+++ hello-spring-cloud-admin/src/main/java/com/funtl/hello/spring/cloud/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package com.funtl.hello.spring.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaClient//服务提供者
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
